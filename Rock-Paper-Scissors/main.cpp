3f626a85fdb75c005843638a2bb0f8ed38ff9aa2
WizzPizz <alvinlundgrenn@gmail.com>
2025-07-12 00:47:03 +0200

    Update

---
+++ Rock-Paper-Scissors/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main () {
+
+//string Rock = "Rock";
+//string Paper = "Paper";
+//string Scissors = "Scissors";
+
+cout << "Hello";
+
+int userInput;
+cout << "Rock, paper or scissors?";
+cin >> userInput;
+cout << "You threw " << userInput; 
+}
